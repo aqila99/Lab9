86adbb81d52fe3513c5040c135994cf6297dd370
aqila99 <aqilahazira1@gmail.com>
2022-06-22 01:09:09 +0800

    commit

---
+++ next_page.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+echo "<html>";
+
+$sid = session_id();
+echo "Session ID returned by session_id(): ".$sid."\n";
+$sid = SID;
+echo "Session ID returned by SID: ".$sid."\n";
+
+$myLogin = $_SESSION["myLogin"];
+print("Value of myLogin has been retrieved: ".$myLogin."\n");
+$myColor = $_SESSION["myColor"];
+echo "Value of myColor has been retrieved: ".$myColor."\n";
+
+echo "</html>\n";
+?>
